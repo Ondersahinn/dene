3dc493b89165e4b3adad371443963489bd4afa8e
Ondersahinn <ondershin@gmail.com>
2019-05-30 00:08:23 +0300

    Ruby Blog

--- app/models/yazilar.rb
+++ app/models/yazilar.rb
@@ -1,6 +1,6 @@
 class Yazilar < ApplicationRecord
     has_many :comments, dependent: :destroy
-        has_attached_file :resim, styles: { medium: "600x400>", thumb: "800x600>" }, default_url: "/images/:style/missing.png" 
+        has_attached_file :resim, styles: { medium: "600x400>", thumb: "150x100>" }, default_url: "/images/:style/missing.png" 
     validates_attachment_content_type :resim, content_type: /\Aimage\/.*\z/
     validates_length_of :aciklama, minimum: 50, allow_blank: true
    
